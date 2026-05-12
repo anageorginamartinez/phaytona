4752fbf9d4517fcccda41aedfa75870c5091adeb
gina <anageorginamartinezmedellin@gmail.com>
2026-05-12 10:18:52 -0600

    es mi examen
    con Camila

---
+++ pro2.py
@@ -0,0 +1,16 @@
+numero = int(input("dame un numero: "))
+
+factorial = 1
+
+if numero <= 0:
+    print("factorial de numero no existe")
+   
+else:
+    while (1, numero + 1):
+        factorial = factorial * 1
+
+    print("El factorial de", numero, "es:", factorial)
+
+
+
+
